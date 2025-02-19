c6f40a3e833ee69386a43a9a249aab78cd54b6de
taltech-courses <98915786+attaltech@users.noreply.github.com>
2025-02-19 23:31:33 +0200

    Create task.py

---
+++ task.py
@@ -0,0 +1,6 @@
+import json
+import itertools
+
+def parse(json-file-name):
+  '''this function takes a json file as an input and returns a list of letters that are recognized with at least confidence level of at least 0.1'''
+  return letters
